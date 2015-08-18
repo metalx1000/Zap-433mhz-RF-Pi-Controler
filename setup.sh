74467dc0f083ac379253361160cac39a3358fa11
metalx1000 <metalx1000@users.noreply.github.com>
2015-08-18 14:35:02 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,17 +5,17 @@ if [[ $EUID -ne 0 ]]; then
   echo "You must be a root user"
   echo "Trying to restart script as sudo"
   sudo $0 $@
-  exit 
+  exit
 fi
 
 echo "Setup for Raspberry Pi"
 
 apt-get update &&
-apt-get upgrade -y && 
+apt-get upgrade -y &&
 apt-get install php5 libapache2-mod-php5 apache2 git-core vim -y
 
-mkdir -p ~/www
-ln -s ~/www /var/www/pi
+mkdir -p /home/pi/www
+ln -s /home/pi/www /var/www/pi
 
 #install wiringPi
 git clone git://git.drogon.net/wiringPi
@@ -23,7 +23,7 @@ cd wiringPi
 ./build
 
 #get RFoutlet Code
-cd ~/www
+cd /home/pi/www
 git clone https://github.com/metalx1000/Zap-433mhz-RF-Pi-Controler.git
 mv Zap-433mhz-RF-Pi-Controler outlet
 cd outlet
@@ -31,8 +31,10 @@ cd outlet
 echo "Starting RFSniffer..."
 echo "press buttons on remote"
 echo "Write this info down"
-echo "Ctrl+C when done"
-./RFSniffer
+echo "<Enter> when done"
+./RFSniffer &
+read
+killall RFSniffer
 
 echo "change mySwitch.setPulseLength(xxx);"
 echo "to match your pulse recorded in last step"
@@ -54,5 +56,4 @@ vim toggle.php
 
 ip="$(ifconfig|grep eth0 -A 1|cut -d\: -f2|tail -n1|awk '{print $1}')"
 echo "Now go to http://$ip/pi/outlet"
-
 
